1ab60f74c8daea42ce32f2709e1a62e668936fa3
Kyle Bunn <kylejbunn@gmail.com>
2022-02-19 21:07:16 -0500

    updated notes

--- Snek.py
+++ Snek.py
@@ -5,7 +5,11 @@ import random
 import pygame
 import tkinter as tk
 from tkinter import messagebox
- 
+
+
+#Class cube
+#Basic game object for both sacks and sneks
+#uses X Y coordinates to record and update its position
 class cube(object):
     rows = 20
     w = 500
@@ -15,17 +19,19 @@ class cube(object):
         self.dirny = 0
         self.color = color
  
-       
+    #move using standard x y coordinates
     def move(self, dirnx, dirny):
         self.dirnx = dirnx
         self.dirny = dirny
         self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
- 
+    
+    #draw the square at the curent position
     def draw(self, surface, eyes=False):
         dis = self.w // self.rows
         i = self.pos[0]
         j = self.pos[1]
  
+        #pygame definitions of cube 
         pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
         if eyes:
             centre = dis//2
@@ -37,24 +43,29 @@ class cube(object):
        
  
  
- 
+#Class snek or "snake" for the less cultured
+#A series of connected cubes that extends as the game is played
+#and has a constant momentumn
 class snek(object):
     body = []
     turns = {}
+    #define snek using cubes
     def __init__(self, color, pos):
         self.color = color
         self.head = cube(pos)
         self.body.append(self.head)
         self.dirnx = 0
         self.dirny = 1
- 
+    
+    #move snek
     def move(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
  
             keys = pygame.key.get_pressed()
- 
+
+            #definitions of arrow keys as game controls
             for key in keys:
                 if keys[pygame.K_LEFT]:
                     self.dirnx = -1
@@ -75,7 +86,8 @@ class snek(object):
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
- 
+
+        #Pressing keys changes cube positions
         for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
@@ -90,7 +102,7 @@ class snek(object):
                 elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
                 else: c.move(c.dirnx,c.dirny)
        
- 
+    #reset snek for new games
     def reset(self, pos):
         self.head = cube(pos)
         self.body = []
@@ -99,7 +111,7 @@ class snek(object):
         self.dirnx = 0
         self.dirny = 1
  
- 
+    #make more snek
     def addCube(self):
         tail = self.body[-1]
         dx, dy = tail.dirnx, tail.dirny
@@ -116,7 +128,7 @@ class snek(object):
         self.body[-1].dirnx = dx
         self.body[-1].dirny = dy
        
- 
+    #draw snek
     def draw(self, surface):
         for i, c in enumerate(self.body):
             if i ==0:
@@ -127,18 +139,21 @@ class snek(object):
  
 
        
- 
+#redrawWindow
+#update the game window to reflect changes in game state
 def redrawWindow(surface):
     global rows, width, s, snack
-    surface.fill((0,0,0))
+    #fancy grass jpg background converted as per pygame recomendations
     background_image = pygame.image.load("grass.jpg").convert()
+    #build background image
     surface.blit(background_image, [0, 0])
     s.draw(surface)
     snack.draw(surface)
     pygame.display.update()
     
  
- 
+#ramdomSnack
+#pick an unoccupied square at random and put a snack there
 def randomSnack(rows, item):
  
     positions = item.body
@@ -153,7 +168,7 @@ def randomSnack(rows, item):
        
     return (x,y)
  
- 
+#Message box for loosing game state and try again
 def message_box(subject, content):
     root = tk.Tk()
     root.attributes("-topmost", True)
@@ -166,37 +181,51 @@ def message_box(subject, content):
  
  
 def main():
+    #global varibles that define key game elements
     global width, rows, s, snack
     width = 500
     rows = 20
     
+    #Call key varibles for game loop
+
+    #win defines game window
     win = pygame.display.set_mode((width, width))
+    #s adds a snek to gamestate
     s = snek((255,0,0), (10,10))
+    #snack adds a snack to gamestate
     snack = cube(randomSnack(rows, s), color=(0,255,0))
+    #flag starts and stops game loop
     flag = True
 
     
     
- 
+    #set clock
     clock = pygame.time.Clock()
    
+   
     while flag:
-        
+        #primary game loop
+        #move the snek around to eat snacks
         pygame.time.delay(50)
         clock.tick(10)
         s.move()
+        #if you eat a snack your snek grows by 1 cube
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = cube(randomSnack(rows, s), color=(0,255,0))
- 
+        #secondary game loop
+        #if you eat yourself the game is over
         for x in range(len(s.body)):
             if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                #score prints in terminal using body legenth as score
                 print('Score: ', len(s.body))
+                #play again message
                 message_box('You Lost!', 'Play again...')
+                #reset button
                 s.reset((10,10))
                 break
  
-           
+        #update game state   
         redrawWindow(win)
  
        
